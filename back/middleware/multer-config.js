f5cc3e85f8eb212bc9c3048c8e3f61af20f4952b
Axel75018 <axel.calvet@gmail.com>
2023-05-16 12:22:17 +0200

    commentaire v2

--- back/middleware/multer-config.js
+++ back/middleware/multer-config.js
@@ -1,17 +1,20 @@
 const multer = require('multer'); // Importation du module multer pour la gestion des fichiers
 
-const MIME_TYPES = { // Dictionnaire des extensions d'images correspondant aux types MIME
+const MIME_TYPES = { // objet Dictionnaire des extensions d'images correspondant aux types MIME
     'image/jpg': 'jpg',
     'image/jpeg': 'jpg',
     'image/png': 'png'
 };
 
-const storage = multer.diskStorage({ // Configuration du stockage des fichiers
+const storage = multer.diskStorage({ // Storage objet configure stockage des fichiers par multer
+    // multer fonction diskstorage methode  qui va définir destination et filename
     destination: (req, file, callback) => { // Destination des fichiers (répertoire dans lequel ils seront enregistrés)
         callback(null, 'images');
     },
     filename: (req, file, callback) => { // Nom du fichier
-        const name = file.originalname.split(' ').join('_'); // Remplacement des espaces dans le nom de fichier par des underscores
+        //split casse la chaine sur les espaces pour en faire un tableau
+        // join transforme le tableau en une chaine ou les partie sont jointes par _
+        const name = file.originalname.split(' ').join('_'); 
         const extension = MIME_TYPES[file.mimetype]; // Récupération de l'extension du fichier à partir du type MIME
         callback(null, name + Date.now() + '.' + extension); // Construction du nom de fichier avec un timestamp pour éviter les doublons
     }
